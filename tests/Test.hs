b54e20651276483e11853572b97beb8b9a2f5154
Alexandr Ruchkin <voidex@live.com>
2015-01-09 23:32:13 +0300

    Fixed test: `hdocs` is not registered while performing a test, so we can't grab docs for tests/Test.hs вЂ” there are no HDocs modules yet Added data/Test.hs

--- tests/Test.hs
+++ tests/Test.hs
@@ -16,11 +16,11 @@ main :: IO ()
 main = runErrorT main' >>= either (\e -> putStrLn e >> exitFailure) (\_ -> exitSuccess) where
 	main' :: ErrorT String IO ()
 	main' = do
-		sdocs <- fmt $ liftM snd $ readSource [] "tests/Test.hs"
-		check "Documentation for main"
-			(M.lookup "main" sdocs == Just "This is main function")
+		sdocs <- fmt $ liftM snd $ readSource [] "tests/data/Test.hs"
+		check "Documentation for file: Test.hs, test"
+			(M.lookup "test" sdocs == Just "This is test function with documentation")
 		edocs <- fmt $ moduleDocs [] "Prelude"
-		check "Documentation for Prelude.null"
+		check "Documentation for installed module: Prelude.null"
 			(M.lookup "null" edocs == Just "Test whether a list is empty.")
 		where
 			check str p = if p then return () else throwError str
---
+++ tests/data/Test.hs
@@ -0,0 +1,5 @@
+module Test (test) where
+
+-- | This is test function with documentation
+test :: ()
+test = ()
