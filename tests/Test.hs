1c434f1eb3829a598a5da9fd89073efa4372b5d1
voidex <voidex@live.com>
2015-04-30 22:20:17 +0300

    Fixed test, version 4.2.1

--- tests/Test.hs
+++ tests/Test.hs
@@ -3,7 +3,7 @@ module Main (
 	) where
 
 import Control.Monad
-import Control.Monad.Error
+import Control.Monad.Except
 import qualified Data.Map as M
 
 import System.Exit
@@ -13,15 +13,17 @@ import HDocs.Haddock
 
 -- | This is main function
 main :: IO ()
-main = runErrorT main' >>= either (\e -> putStrLn e >> exitFailure) (const exitSuccess) where
-	main' :: ErrorT String IO ()
+main = runExceptT main' >>= either (\e -> putStrLn e >> exitFailure) (const exitSuccess) where
+	main' :: ExceptT String IO ()
 	main' = do
 		sdocs <- fmt $ liftM snd $ readSource [] "tests/HelpTest.hs"
 		check "Documentation for file: Test.hs, test"
 			(M.lookup "test" sdocs == Just "This is test function with documentation")
 		edocs <- fmt $ moduleDocs [] "Prelude"
 		check "Documentation for installed module: Prelude.null"
-			(M.lookup "null" edocs == Just "Test whether a list is empty.")
+			(M.lookup "null" edocs `elem`
+				[Just "Test whether a list is empty.",
+				Just "Test whether the structure is empty. The default implementation is\n optimized for structures that are similar to cons-lists, because there\n is no general way to do better."])
 		where
 			check str p = unless p $ throwError str
 			fmt = liftM formatDocs
