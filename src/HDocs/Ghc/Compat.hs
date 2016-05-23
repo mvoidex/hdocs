24cc63bbb59648324a234b74586f72fb4223d4f7
voidex <voidex@live.com>
2016-05-24 00:42:58 +0300

    Replace endif/if with elif

--- src/HDocs/Ghc/Compat.hs
+++ src/HDocs/Ghc/Compat.hs
@@ -15,40 +15,35 @@ import Exception (ExceptionMonad)
 pkgDatabase :: GHC.DynFlags -> Maybe [GHC.PackageConfig]
 #if __GLASGOW_HASKELL__ == 800
 pkgDatabase = fmap (concatMap snd) . GHC.pkgDatabase
-#endif
-#if __GLASGOW_HASKELL__ == 710
+#elif __GLASGOW_HASKELL__ == 710
 pkgDatabase = GHC.pkgDatabase
 #endif
 
 #if __GLASGOW_HASKELL__ == 800
 cleanupHandler :: GHC.DynFlags -> m a -> m a
 cleanupHandler _ = id
-#endif
-#if __GLASGOW_HASKELL__ == 710
+#elif __GLASGOW_HASKELL__ == 710
 cleanupHandler :: (ExceptionMonad m) => GHC.DynFlags -> m a -> m a
 cleanupHandler = GHC.defaultCleanupHandler
 #endif
 
 #if __GLASGOW_HASKELL__ == 800
 type UnitId = Module.UnitId
-#endif
-#if __GLASGOW_HASKELL__ == 710
+#elif __GLASGOW_HASKELL__ == 710
 type UnitId = Module.PackageKey
 #endif
 
 unitId :: GHC.PackageConfig -> UnitId
 #if __GLASGOW_HASKELL__ == 800
 unitId = GHC.unitId
-#endif
-#if __GLASGOW_HASKELL__ == 710
+#elif __GLASGOW_HASKELL__ == 710
 unitId = GHC.packageKey
 #endif
 
 moduleUnitId :: Module.Module -> UnitId
 #if __GLASGOW_HASKELL__ == 800
 moduleUnitId = Module.moduleUnitId
-#endif
-#if __GLASGOW_HASKELL__ == 710
+#elif __GLASGOW_HASKELL__ == 710
 moduleUnitId = Module.modulePackageKey
 #endif
 
